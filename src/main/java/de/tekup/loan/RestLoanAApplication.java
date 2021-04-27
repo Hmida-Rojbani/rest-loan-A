bbe25e5cce98e991f45ab624696ae0e37ca2f06c
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-27 12:52:02 +0100

    init

---
+++ src/main/java/de/tekup/loan/RestLoanAApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLoanAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLoanAApplication.class, args);
+	}
+
+}
