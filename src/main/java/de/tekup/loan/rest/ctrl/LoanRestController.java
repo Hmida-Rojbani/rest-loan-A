3d0a1f97c546f1a6843b021151d61c933cab541a
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-27 14:11:33 +0100

    Update LoanRestController.java

--- src/main/java/de/tekup/loan/rest/ctrl/LoanRestController.java
+++ src/main/java/de/tekup/loan/rest/ctrl/LoanRestController.java
@@ -14,14 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 import de.tekup.loan.rest.models.CustomerRequest;
 import de.tekup.loan.rest.models.WsResponse;
 import de.tekup.loan.rest.service.LoanService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "LoanEligebilty", description = "Service to check eligeblity of client to get a bank loan")
 public class LoanRestController {
 	
 	@Autowired
 	private LoanService service;
 	
+	@ApiOperation(value = "check customer for a loan", response = WsResponse.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfuly you get a response")
+	})
 	@PostMapping(path="/get-status", produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE}
 							, consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<WsResponse> getLoanCheck(@RequestBody CustomerRequest request){
